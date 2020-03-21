d00a7769aa221a8e9699a59721c65e6e5d1a3bc1
SLatyankov <Slatiankov@yandex.ru>
2020-03-22 00:15:19 +0400

    fgjh

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,3 +2,9 @@ module.exports = function createDreamTeam(/* members */) {
   throw 'Not implemented';
   // remove line with error and write your code here
 };
+var arr;
+members.foreach(el => {
+  if (typeof el === 'string') {
+
+  }
+} )
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,13 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    var str1;
+    for (let i = 0; i<options.repeatTimes(key); i++) {
+        str1 += str;
+        for (let i = 0; i < options.additionRepeatTimes(key); i++) {
+            str1 += options.addition(key) + options.additionSeparator(key);
+        }
+        str1.slice(0, -2);
+        str1 +=separator;
+    }
+    str1.slice(0, -2);
+    return str1;
 };
-  
